046a0b9664bfba0299ddb5f1834c49b588630a47
Mateusz Motyl <motyl.mateusz1999@gmail.com>
2021-02-15 19:26:28 +0100

    asd

--- src/domain/AdminPage/Dashboard/Dashboard.tsx
+++ src/domain/AdminPage/Dashboard/Dashboard.tsx
@@ -6,15 +6,13 @@ import styles from './Dashboard.module.scss';
 const Dashboard = (): JSX.Element => {
   const content = (
     <div>
-      Projekt ma na celu stworzenie aplikacji do przeprowadzania quizów. Prowadzący logując się do panelu może z jego poziomu tworzyć nowe quizy wprowadzając pytania wraz z określoną liczbą punktów za poprawną odpowiedź.
-      <br/>Następnie określa termin rozpoczęcia i zakończenia quizu i wprowadza adresy
-      e-mail na które rozesłany ma zostać specjalnie wygenerowany, unikatowy dla każdego studenta link aktywacyjny do quizu.
-      <br/>
-      <br/>
-      Prowadzący po przeprowadzonym quizie ma dostęp do wyników poszczególnych studentów, a także do zbiorowych wyników, żeby móc łatwiej zorientować się jakie wyniki osiągneła cała grupa.
-<br/>
-<br/>
-      Student otrzymuje na swoją skrzynkę pocztową link do quizu, który dostępny jest dopiero w czasie który został określony przez prowadzącego wprowadzającego quiz. Po zakończeniu quizu studentowi wyświetlana jest ilość zdobytych przez niego punktów.
+      Celem projektu było stworzenie aplikacji która umożliwi stworzenie i przeprowadzenie quizu.
+      <br/><br/>
+      Aplikacja posiada interfejs w którym w łatwy sposób można wprowadzić pytania dla danego zagadnienia, zaznaczyć która z odpowiedzi jest prawidłowa oraz ustawić ilosć punktów do zdobycia za dobrą odpowiedź. Pytania te można w łatwy sposób modyfikować czy też usunąć całą pulę.
+      <br/><br/>
+      Administrator ma do dyspozycji również interfejs służący do zaplanowania quizu w którym może określić kiedy ma rozpoczać się quiz, ile czasu będzie miał użytkownik na jego rozpoczęcie a także czas trwania quizu i pytania które mają się w nim zawierać. Administrator wprowadza pulę adresów email na które rozesłane mają być specjalnie wygenerowane, unikalne dla każdego studenta linki aktywacyjne do quizu.
+      <br/><br/>
+      Student po otrzymaniu linku aktywacyjnego na swoją skrzynkę odbiorczą wypełnia quiz. Po zakończeniu quizu wyniki poszczególnych studentów dostępne są dla prowadzącego w zakładce "Historia". W zakładce tej wyświetlane są punkty zdobyte przez studenta, jego odpowiedzi na poszczególne pytania a także status quizu.
     </div>
   );
 
